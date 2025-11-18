1346545fa6c957145a886b2ba11aa2e8816c42f2
Maks Zakharov <mtsovik@bk.ru>
2025-11-18 19:20:06 +0500

    add api/users/

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,7 +1,28 @@
 from rest_framework import serializers
 from .models import Run
+from django.contrib.auth.models import User
+
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'date_joined',
+            'username',
+            'last_name',
+            'first_name',
+            'type',
+        ]
+    type = serializers.SerializerMethodField()
+    
+    def get_type(self, obj):
+        if obj.is_staff:
+            return 'coach'
+        else:
+            return 'athlete'
--- app_run/views.py
+++ app_run/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from django.conf import settings
 from rest_framework import viewsets
 from .models import Run
-from .serializers import RunSerializer
+from .serializers import RunSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 
 @api_view(["GET"])
@@ -19,3 +20,17 @@ def company_details(request):
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_queryset(self):
+        filter_type = self.request.query_params.get('type')
+        if filter_type == 'coach':
+            return User.objects.filter(is_staff=True)
+        if filter_type == 'athlete':
+            return User.objects.filter(is_staff=False)
+        if filter_type == None:
+            return User.objects.all()
+
